fbe7ff83b200cea496b23f9b89a0555f9355d5f9
Sergey <Zaikouski.sergey@gmail.com>
2023-04-04 12:35:55 +0300

    button

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
 import React from 'react';
 import './App.css';
-import {Header} from "./components/header";
-import {Body} from "./components/body";
-import {Footer} from "./components/footer";
+import {Button} from "./components/Button";
 
-type AppType = {
-    state: Array<CarType>
-}
-type CarType = {
-    manufacturer: string,
-    model: string
 
-}
+function App() {
+    const Button1Foo = (subscriber:string, age:number, address:string) => {
+        console.log(subscriber, age, address)
 
-function App(props: AppType) {
+    }
+    const Button2Foo = (subscriber:string) => {
+        console.log(subscriber)
+
+    }
+    const Button3Foo =() =>{
+        console.log("i'm a stupid button")
+    }
+
     return (
         <div className="App">
-            <Header title={"New Body"}/>
-            <Body titleForBody={"NEW BODY"} listCars={props.state}/>
-            <Footer titleForFooter={"New Footer"}/>
+            <Button name={'MyYouTubeChanel-1'} callBack={()=> Button1Foo('im vasja', 21, 'live in Minsk')}/>
+            <Button name={'MyYouTubeChanel-2'} callBack={()=> Button2Foo('im kolja')}/>
+            <Button name={'Stupid Button'} callBack={Button3Foo} />
+
         </div>
     );
 }
